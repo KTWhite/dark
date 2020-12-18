3ee61b2bbb520d507be2643edef108f85e394893
KTWhite <yu_wl_h@163.com>
2020-12-19 05:08:30 +0800

    spring 配置mybatis

--- src/main/java/com/github/dark/mapper/LoginMapper.java
+++ src/main/java/com/github/dark/mapper/LoginMapper.java
@@ -4,8 +4,7 @@ import com.github.dark.entity.User;
 import tk.mybatis.mapper.common.Mapper;
 
 import java.util.List;
-
+@org.apache.ibatis.annotations.Mapper
 public interface LoginMapper extends Mapper<User> {
 
-    List<User> selectAlls();
 }
